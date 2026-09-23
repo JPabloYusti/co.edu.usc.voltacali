8d6a8b16ed6fe65dc2d67a1594bcc2df25ec0f06
Juan Pablo Yusti Chaux <juanpayusti@gmail.com>
2026-09-23 21:01:38 +0000

    CREACION DE ATRIBUTOS

---
+++ parcial1-voltacali/src/main/java/co/edu/usc/voltacali/CargadorVE.java
@@ -0,0 +1,23 @@
+package co.edu.usc.voltacali;
+
+
+public class CargadorVE {
+
+    public enum TipoConector { TIPO_1, TIPO_2, CCS2, CHADEMO, GBT }
+    public enum TipoCargador { MURAL, PEDESTAL, RAPIDO_DC, ULTRARRAPIDO, PORTATIL, BIDIRECCIONAL_V2G }
+    public enum Ubicacion { CENTRO_COMERCIAL, UNIVERSIDAD, ESTACION_SERVICIO, PARQUEADERO_PUBLICO, RESIDENCIAL, HOTEL, TERMINAL, FLOTA_CORPORATIVA }
+
+    private String fabricante;
+    private int anioInstalacion;
+    private int voltajeNominal;
+    private TipoConector tipoConector;
+    private TipoCargador tipoCargador;
+    private int numeroConectores;
+    private int puestosParqueo;
+    private double potenciaMaxima;
+    private Ubicacion ubicacion;
+    private double potenciaActual;
+
+    
+
+    
