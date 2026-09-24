34eeff1200ed38c25e24f00bd1919f089c11c4f0
Juan Pablo Yusti Chaux <juanpayusti@gmail.com>
2026-09-24 18:57:56 +0000

    adelanto

--- parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
+++ parcial1-voltacali/src/main/java/co/edu/usc/voltacali/App.java
@@ -2,14 +2,37 @@ package co.edu.usc.voltacali;
 
 public class App {
     public static void main(String[] args) {
-     
-        CargadorVE c1 = new CargadorVE("ABB", 2023, 400, CargadorVE.TipoConector.CCS2, CargadorVE.TipoCargador.RAPIDO_DC, 2, 2, 60, CargadorVE.Ubicacion.UNIVERSIDAD);
-        CargadorVE c2 = new CargadorVE("Siemens", 2022, 220, CargadorVE.TipoConector.TIPO_2, CargadorVE.TipoCargador.MURAL, 1, 1, 22, CargadorVE.Ubicacion.CENTRO_COMERCIAL);
-        CargadorVE c3 = new CargadorVE("Delta", 2024, 800, CargadorVE.TipoConector.CCS2, CargadorVE.TipoCargador.ULTRARRAPIDO, 2, 2, 150, CargadorVE.Ubicacion.ESTACION_SERVICIO);
-        CargadorVE c4 = new CargadorVE("Wallbox", 2021, 220, CargadorVE.TipoConector.TIPO_2, CargadorVE.TipoCargador.MURAL, 1, 1, 11, CargadorVE.Ubicacion.RESIDENCIAL);
-        CargadorVE c5 = new CargadorVE("Enel X", 2025, 22); 
+        // Parte E: Paso 1 - Crear la flota
+        CargadorVE c1 = new CargadorVE("ABB", 2023, 400, CargadorVE.TipoConector.CCS2, CargadorVE.TipoCargador.RAPIDO_DC, 2, 2, 60.0, CargadorVE.Ubicacion.UNIVERSIDAD);
+        CargadorVE c2 = new CargadorVE("Siemens", 2022, 220, CargadorVE.TipoConector.TIPO_2, CargadorVE.TipoCargador.MURAL, 1, 1, 22.0, CargadorVE.Ubicacion.CENTRO_COMERCIAL);
+        CargadorVE c3 = new CargadorVE("Delta", 2024, 800, CargadorVE.TipoConector.CCS2, CargadorVE.TipoCargador.ULTRARRAPIDO, 2, 2, 150.0, CargadorVE.Ubicacion.ESTACION_SERVICIO);
+        CargadorVE c4 = new CargadorVE("Wallbox", 2021, 220, CargadorVE.TipoConector.TIPO_2, CargadorVE.TipoCargador.MURAL, 1, 1, 11.0, CargadorVE.Ubicacion.RESIDENCIAL);
+        CargadorVE c5 = new CargadorVE("Enel X", 2025, 22.0); // Usa constructor reducido
 
         CargadorVE[] flota = {c1, c2, c3, c4, c5};
-        System.out.println("Flota creada.");
-    }
-}
+
+        // Paso 2 - Sesión de carga sobre C1
+        System.out.println("[P01] "); c1.setPotenciaActual(40.0); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P02] "); c1.aumentarPotencia(15.0); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P03] Tiempo estimado C1: " + String.format("%.2f", c1.tiempoEstimadoCarga(66.0)) + " horas");
+        System.out.println("[P04] "); c1.aumentarPotencia(10.0); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P05] "); c1.reducirPotencia(30.0); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P06] Tiempo estimado C1: " + String.format("%.2f", c1.tiempoEstimadoCarga(50.0, 2, 15.0)) + " horas");
+        System.out.println("[P07] Tiempo estimado C1: " + String.format("%.2f", c1.tiempoEstimadoCarga(50.0, 40.0)) + " horas");
+        System.out.println("[P08] "); c1.aumentarPotencia(); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P09] "); c1.aumentarPotencia(5.0, 3); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P10] "); c1.reducirPotencia(50.0); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P11] "); c1.cortarCarga(); System.out.println("Potencia C1: " + c1.getPotenciaActual() + " kW");
+        System.out.println("[P12] Tiempo estimado C1: " + String.format("%.2f", c1.tiempoEstimadoCarga(10.0)) + " horas");
+
+        // Paso 3 - Operaciones sobre el resto de la flota
+        System.out.println("\n[P13]");
+        c2.setPotenciaActual(22.0); System.out.println("Potencia C2: " + c2.getPotenciaActual() + " kW");
+        c3.setPotenciaActual(120.0); System.out.println("Potencia C3: " + c3.getPotenciaActual() + " kW");
+        c4.aumentarPotencia(7.4); System.out.println("Potencia C4: " + c4.getPotenciaActual() + " kW");
+        c5.aumentarPotencia(30.0); System.out.println("Potencia C5: " + c5.getPotenciaActual() + " kW");
+
+        // Paso 4 - Estadísticas y validaciones
+        System.out.println("\n[P14]");
+        int[] conteoTipos = CargadorVE.contarPorTipo(flota);
+        for (int i = 0; i < conteoTipos.length; i++) {
--- parcial1-voltacali/src/main/java/co/edu/usc/voltacali/CargadorVE.java
+++ parcial1-voltacali/src/main/java/co/edu/usc/voltacali/CargadorVE.java
@@ -1,8 +1,6 @@
 package co.edu.usc.voltacali;
 
 import java.util.Vector;
-import java.util.Arrays;
-
 
 public class CargadorVE {
 
@@ -10,6 +8,7 @@ public class CargadorVE {
     public enum TipoCargador { MURAL, PEDESTAL, RAPIDO_DC, ULTRARRAPIDO, PORTATIL, BIDIRECCIONAL_V2G }
     public enum Ubicacion { CENTRO_COMERCIAL, UNIVERSIDAD, ESTACION_SERVICIO, PARQUEADERO_PUBLICO, RESIDENCIAL, HOTEL, TERMINAL, FLOTA_CORPORATIVA }
 
+    
     private String fabricante;
     private int anioInstalacion;
     private int voltajeNominal;
@@ -21,11 +20,80 @@ public class CargadorVE {
     private Ubicacion ubicacion;
     private double potenciaActual;
 
+    
     private Vector<RegistroSesion> bitacora;
 
     public static int totalCargadores = 0;
     public static int contadorRegistros = 0;
+    public static final double LIMITE_RED = 50.0;
+    public static final double INCREMENTO_DEFECTO = 5.0;
 
+   
+    public class RegistroSesion {
+        private String evento;
+        private boolean valido;
+        private String fabricanteLog;
+        private int anioInstalacionLog;
+        private double potenciaActualLog;
+        private int consecutivo;
+
+        public RegistroSesion(String evento, boolean valido) {
+            this.evento = evento;
+            this.valido = valido;
+            
+            this.fabricanteLog = CargadorVE.this.fabricante;
+            this.anioInstalacionLog = CargadorVE.this.anioInstalacion;
+            this.potenciaActualLog = CargadorVE.this.potenciaActual;
+            
+            CargadorVE.contadorRegistros++;
+            this.consecutivo = CargadorVE.contadorRegistros;
+        }
+
+        public void describir() {
+            System.out.println("Reg #" + consecutivo + " | " + fabricanteLog + " (" + anioInstalacionLog + ") | " + evento + " | Valido: " + valido + " | Pot: " + potenciaActualLog + " kW");
+        }
+        
+        public double getPotenciaActualLog() { return potenciaActualLog; }
+        public boolean isValido() { return valido; }
     }
 
+  
+    public CargadorVE(String fabricante, int anioInstalacion, int voltajeNominal, TipoConector tipoConector, TipoCargador tipoCargador, int numeroConectores, int puestosParqueo, double potenciaMaxima, Ubicacion ubicacion) {
+        this.fabricante = fabricante;
+        this.anioInstalacion = anioInstalacion;
+        this.voltajeNominal = voltajeNominal;
+        this.tipoConector = tipoConector;
+        this.tipoCargador = tipoCargador;
+        this.numeroConectores = numeroConectores;
+        this.puestosParqueo = puestosParqueo;
+        this.potenciaMaxima = potenciaMaxima;
+        this.ubicacion = ubicacion;
+        this.potenciaActual = 0.0;
+        this.bitacora = new Vector<>();
+        totalCargadores++; 
+    }
     
+    public CargadorVE(String fabricante, int anioInstalacion, double potenciaMaxima) {
+        this(fabricante, anioInstalacion, 220, TipoConector.TIPO_2, TipoCargador.PEDESTAL, 1, 1, potenciaMaxima, Ubicacion.PARQUEADERO_PUBLICO);
+    }
+
+
+    public CargadorVE(CargadorVE otro) {
+        this(otro.fabricante, otro.anioInstalacion, otro.voltajeNominal, otro.tipoConector, otro.tipoCargador, otro.numeroConectores, otro.puestosParqueo, otro.potenciaMaxima, otro.ubicacion);
+    }
+
+ 
+    public String getFabricante() { return fabricante; }
+    public void setFabricante(String fabricante) { this.fabricante = fabricante; }
+    public int getAnioInstalacion() { return anioInstalacion; }
+    public void setAnioInstalacion(int anioInstalacion) { this.anioInstalacion = anioInstalacion; }
+    public int getVoltajeNominal() { return voltajeNominal; }
+    public void setVoltajeNominal(int voltajeNominal) { this.voltajeNominal = voltajeNominal; }
+    public TipoConector getTipoConector() { return tipoConector; }
+    public void setTipoConector(TipoConector tipoConector) { this.tipoConector = tipoConector; }
+    public TipoCargador getTipoCargador() { return tipoCargador; }
+    public void setTipoCargador(TipoCargador tipoCargador) { this.tipoCargador = tipoCargador; }
+    public int getNumeroConectores() { return numeroConectores; }
+    public void setNumeroConectores(int numeroConectores) { this.numeroConectores = numeroConectores; }
+    public int getPuestosParqueo() { return puestosParqueo; }
+  
